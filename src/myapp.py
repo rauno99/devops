3854bea8770c6273f72efe18395a21af18bdb102
rauno99 <rauno.polluaar@gmail.com>
2022-02-15 16:51:25 +0200

    Initial commit

---
+++ src/myapp.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello, World!"
+
+# @app.route("/ping")
+# def pong():
+#     return "Pong"
+
+if __name__ == "__main__":
+    app.run(debug=True)
